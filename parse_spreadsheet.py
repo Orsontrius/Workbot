3c4a29f7667dfbaea582a1263837737841ef025f
Orsontrius <orsontrius@gmail.com>
2020-07-17 12:29:30 -0400

    continued work on spreadsheet parsing

--- parse_spreadsheet.py
+++ parse_spreadsheet.py
@@ -1,10 +1,34 @@
 import openpyxl
-file = "2-15 July 2020_v3.xlsx"
+import re
+
+file = "16-22 July 2020_v1.xlsx"
 
 def parse_spreadsheet(file):
     wb = openpyxl.load_workbook(filename=file)
     ws = wb.active
 
-    print(ws["A"])
+    #get the row numbers for the date and my row
+    dateRow = 3
+    cjRow = 0
+    x = 1
+    for row in ws["A"]:
+        if row.value == "CJ":
+            cjRow = x 
+        x += 1
+
+    for i in ws.iter_rows(min_row=cjRow, max_row=cjRow, values_only=True):
+        times = i
+    for i in ws.iter_rows(min_row=dateRow, max_row=dateRow, values_only=True):
+        dates = i
+
+
+    for i in range(len(times)):
+        if re.search("[am]|[pm]", str(times[i])):
+            
+
+    
+
+
+    
 
 parse_spreadsheet(file)
